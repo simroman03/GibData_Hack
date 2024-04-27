b84556ad7654bd50cc6692d7ec450a7b2727bf87
simroman03 <126416968+simroman03@users.noreply.github.com>
2024-04-28 00:09:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
-import streamlit_shadcn_ui as ui
+#import streamlit_shadcn_ui as ui
 
 
 st.set_page_config(
